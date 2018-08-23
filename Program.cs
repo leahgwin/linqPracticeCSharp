4a1c624b72bb9f925e788ea505390abaf9d366fe
leahgwin <leahgwin@gmail.com>
2018-08-23 13:37:41 -0500

    added partition exercise

--- Program.cs
+++ Program.cs
@@ -97,11 +97,53 @@ namespace linq
                 };
             Console.WriteLine(prices.Max().ToString("C"));
 
-
-
             //PARTITIONING-----------------------------------------------------------------
 
+            /*
+                Store each number in the following List until a perfect square
+                is detected.
 
+                Ref: https://msdn.microsoft.com/en-us/library/system.math.sqrt(v=vs.110).aspx
+            */
+            List<int> wheresSquaredo = new List<int>()
+                    {
+                        66, 12, 8, 27, 82, 34, 7, 50, 19, 46, 81, 23, 30, 4, 68, 14
+                    };
+
+            List<int> nonSquares = wheresSquaredo.TakeWhile(n => Math.Sqrt(n) % 1 != 0).ToList();
+            foreach (var item in nonSquares)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            //CUSTOM TYPES-----------------------------------------------------------------
+
+            // Build a collection of customers who are millionaires
+            // public class Customer
+            // {
+            //     public string Name { get; set; }
+            //     public double Balance { get; set; }
+            //     public string Bank { get; set; }
+            // }
+
+            // public class Program
+            // {
+            //     public static void Main()
+            //     {
+            //         List<Customer> customers = new List<Customer>() {
+            //     new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+            //     new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+            //     new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+            //     new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+            //     new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+            //     new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+            //     new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+            //     new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+            //     new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+            //     new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            // };
+            //     }
+            // }
 
         }
     }
