1ab4737475313366eb93cccd04ae36342714c963
leahgwin <leahgwin@gmail.com>
2018-08-23 11:56:46 -0500

    finished the ordering linq practices

--- Program.cs
+++ Program.cs
@@ -21,13 +21,43 @@ namespace linq
                 Console.WriteLine($"{fruit}");
             }
 
-            // Which of the following numbers are multiples of 4 or 6
-            List<int> numbers = new List<int>()
-{
-    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
-};
+            // Order these student names alphabetically, in descending order (Z to A)
+            List<string> names = new List<string>()
+                {
+                    "Heather", "James", "Xavier", "Michelle", "Brian", "Nina",
+                    "Kathleen", "Sophia", "Amir", "Douglas", "Zarley", "Beatrice",
+                    "Theodora", "William", "Svetlana", "Charisse", "Yolanda",
+                    "Gregorio", "Jean-Paul", "Evangelina", "Viktor", "Jacqueline",
+                    "Francisco", "Tre"
+                };
 
-            List<int> fourSixMultiples = numbers.Where();
+            var descend = from name in names
+                          orderby name descending
+                          select name;
+
+            foreach (string name in descend)
+            {
+                Console.WriteLine($"{name}");
+            }
+
+            // Build a collection of these numbers sorted in ascending order
+            List<int> numbers = new List<int>()
+                {
+                    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+                };
+
+            var descendNum = from num in numbers
+                             orderby num ascending
+                             select num;
+
+            foreach (int num in descendNum)
+            {
+                Console.WriteLine($"{num}");
+            }
+
+
+
+
 
         }
     }
