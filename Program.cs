04b618234ad46e08d82fd631c207b298a7deaa76
leahgwin <leahgwin@gmail.com>
2018-08-23 11:19:19 -0500

    finished first linq practice

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                                          where fruit.StartsWith("L")
+                                          orderby fruit ascending
+                                          select fruit;
+
+            foreach (string fruit in LFruits)
+            {
+                Console.WriteLine($"{fruit}");
+            }
+
+            // Which of the following numbers are multiples of 4 or 6
+            List<int> numbers = new List<int>()
+{
+    15, 8, 21, 24, 32, 13, 30, 12, 7, 54, 48, 4, 49, 96
+};
+
+            List<int> fourSixMultiples = numbers.Where();
+
+        }
+    }
+}
